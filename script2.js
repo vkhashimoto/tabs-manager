7bbf15d9a840ef59daad8e1417db8934224bd2e6
Rodrigo Almeida <35853479+RodrigoDisselli@users.noreply.github.com>
2020-01-14 13:35:45 -0300

    script para criação de lista adicionado

--- script2.js
+++ script2.js
@@ -1,1 +1,22 @@
-document.getElementById("pages-list-id").innerHTML = "<li> <div class='img' style='background-image: url(icon.png);'></div> <div><h3>Page Title</h3><h4>www.google.com.br/vini-otariohasuhsahuhdhsaudhduadsadh</h4></div></li>"
+var database = [{"pageTitle": "Google"},{"pageTitle": "Youtube"},{"pageTitle": "Batata doce"},{"pageTitle": "Medium - Artigo"},];
+const pagesList = document.getElementById("pages-list-id");
+
+
+database.map(object => createListItem(object.pageTitle));
+
+// database.filter(a => a.pageTitle === 'Google');
+
+function createListItem(pageTitle) {
+    var li = document.createElement("LI");
+    li.innerHTML = `
+        <div class='img' style='background-image: url(icon.png);'></div> 
+        <div>
+            <h3>${pageTitle}</h3>
+            <h4>www.google.com.br/vini-otariohasuhsahuhdhsaudhduadsadh</h4>
+         </div>`;
+    pagesList.appendChild(li);
+}
+
+
+
+// document.getElementById("pages-list-id").innerHTML = "<li> <div class='img' style='background-image: url(icon.png);'></div> <div><h3>Page Title</h3><h4>www.google.com.br/vini-otariohasuhsahuhdhsaudhduadsadh</h4></div></li>"
